82f7aaa9f5f7ab272c214b4d32066a94c860afa5
Pedro Bonilla <p.bonilla.silio@hotmail.com>
2020-09-30 12:45:13 +0200

    Add files via upload

---
+++ ps_container_creation_variables_mssql.ps1
@@ -0,0 +1,7 @@
+﻿# Env variables to be used on a later creation of a mssql container for testing
+$env:mssql_container_eula = 'Y'
+$env:mssql_container_sa_passw = 'Sql_1234'
+$env:mssql_container_agent = 'true'
+$env:mssql_container_name = 'bonisql'
+$env:mssql_container_port = '1433:1433'
+$env:mssql_container_image = 'microsoft/mssql-server-windows-developer'
